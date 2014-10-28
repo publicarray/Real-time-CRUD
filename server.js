8046f6de08c8adb07d36fbc0dc264f60c65592fe
Sebastian Schmidt <publicarray@icloud.com>
2014-10-28 17:20:59 +1000

    return friendly 404 errors

--- server.js
+++ server.js
@@ -133,3 +133,12 @@ function escapeHtml(text) {
     return 0;
   }
 }
+
+// return friendly 404 errors
+app.use(function(req, res, next){
+  res.status(404);
+  if (req.accepts('html')) {
+    res.render('404', {url: req.url});
+    return;
+  }
+});
